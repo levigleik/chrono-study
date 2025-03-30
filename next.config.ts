6d1febb5f9937467133384e3405444d7296d263b
Levi <leviacedo1@gmail.com>
2025-03-30 14:04:49 -0300

    feat: added pwa

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from 'next'
+import withSerwistInit from '@serwist/next'
 
-const nextConfig: NextConfig = {
-  eslint: {
-    // ignoreDuringBuilds: true,
-  },
-}
+const withSerwist = withSerwistInit({
+  swSrc: 'src/app/sw.ts',
+  swDest: 'public/sw.js',
+})
 
-export default nextConfig
+export default withSerwist({})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,44 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { Providers } from './providers'
 
+const APP_NAME = 'Chrono Study'
+const APP_DEFAULT_TITLE = 'Chrono Study'
+const APP_TITLE_TEMPLATE = '%s - Chrono Study'
+const APP_DESCRIPTION = 'Uma ferramenta para estudantes'
+
 export const metadata: Metadata = {
-  title: 'Chrono Study',
-  description: 'Uma ferramenta para estudantes.',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 }
 
 export default function RootLayout({
